362f9d4bba31cb9fd5408c36e6b139f13edac279
mrgrges <33262474+mrgrges@users.noreply.github.com>
2024-10-17 21:50:29 -0400

    Finalized substraction calculator code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter first number to substract: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number to substract: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int difference = num1 - num2;
+
+Console.WriteLine($"The difference between {num1} and {num2} = {difference}");
